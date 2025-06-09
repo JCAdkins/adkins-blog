d7d2efb20ad01e587dd9859dcf6247a215cfb5b2
Jordan Adkins <jordan.adkins111@gmail.com>
2025-06-08 23:19:52 -0700

    Added a word of the day feature to the main page. I'm experimenting with _templated and seeing what it can do and if it could help make the photos look better; I probably won't keep the editor.

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,20 +1,24 @@
 import BlogAdminCard from "@/components/cards/blog-admin-card";
 import BlogCardServer from "@/components/cards/blog-card-server";
+import PhotoEditor from "@/components/editor";
 import { getAllBlogs } from "@/lib/db/queries";
 import { Blog } from "next-auth";
 
 const Admin = async () => {
   const blogs: Blog[] | null = await getAllBlogs();
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {blogs?.map((blog, ind) => (
-        <BlogCardServer
-          key={ind}
-          blog={blog}
-          CardComponent={BlogAdminCard}
-          extraProps={{ canEdit: true }}
-        />
-      ))}
+    // <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+    //   {blogs?.map((blog, ind) => (
+    //     <BlogCardServer
+    //       key={ind}
+    //       blog={blog}
+    //       CardComponent={BlogAdminCard}
+    //       extraProps={{ canEdit: true }}
+    //     />
+    //   ))}
+    // </div>
+    <div className="flex h-max w-full justify-center">
+      <PhotoEditor />
     </div>
   );
 };
